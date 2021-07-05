a42318d1bc09829a81a6e0c17d26317a309a85e2
Shav <shavhugan@gmail.com>
2021-07-05 23:00:39 +0100

    added the add post box, functionaliy still not there however

---
+++ client/index.js
@@ -0,0 +1,7 @@
+// Setup 
+const modal = document.querySelector('.main-modal');
+const closeButton = document.querySelectorAll('.modal-close');
+
+// Bind Event Listeners
+
+
---
+++ client/main.js
@@ -0,0 +1,40 @@
+const modalClose = () => {
+modal.classList.remove('fadeIn');
+modal.classList.add('fadeOut');
+setTimeout(() => {
+    modal.style.display = 'none';
+}, 500);
+}
+
+const modalClose = () => {
+    modal.classList.remove('fadeIn');
+    modal.classList.add('fadeOut');
+    setTimeout(() => {
+        modal.style.display = 'none';
+    }, 500);
+}
+
+const openModal = () => {
+    modal.classList.remove('fadeOut');
+    modal.classList.add('fadeIn');
+    modal.style.display = 'flex';
+}
+
+
+for (let i = 0; i < closeButton.length; i++) {
+
+    const elements = closeButton[i];
+
+    elements.onclick = (e) => modalClose();
+
+    modal.style.display = 'none';
+
+    window.onclick = function (event) {
+        if (event.target == modal) modalClose();
+    }
+}
+
+module.exports = {
+    modalClose,
+    openModal,
+}
