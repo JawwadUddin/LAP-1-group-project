2612ba4ad0b9294b45eed54f7f04379defccc982
ridwan.axmed@gmail.com <ridwan.axmed@gmail.com>
2021-07-06 10:47:36 +0100

    commenting system completed

--- server/journal_class.js
+++ server/journal_class.js
@@ -1,9 +1,11 @@
 class JournalEntry {
-    constructor(id,entry,date,time) {
+    constructor(id,entry,date,time,comment) {
         this.id = id 
         this.entry = entry
         this.date = date  
         this.time = time 
+        this.tag = tag
+        this.comment = comment
     }
 }
 
