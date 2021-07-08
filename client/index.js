de0c3bb078f26527dad966af53ca333c42fa640c
JawwadUddin <m.m.j.uddin12@gmail.com>
2021-07-08 16:44:31 +0100

    fix

--- client/index.js
+++ client/index.js
@@ -1,20 +1,20 @@
-const formgif = document.querySelector('#searchbargif');
+// const formgif = document.querySelector('#searchbargif');
 
-// Bind Event Listeners
-formgif.addEventListener('submit', searchgiphy);
-//closes modals on opening the site 
+// // Bind Event Listeners
+// formgif.addEventListener('submit', searchgiphy);
+// //closes modals on opening the site 
 
-all_modals = ['addPost-modal', 'randomPost-modal']
-all_modals.forEach((modal)=>{
-    const modalSelected = document.querySelector('.'+modal);
-    modalSelected.classList.remove('fadeIn');
-    modalSelected.classList.add('fadeOut');
-    modalSelected.style.display = 'none';
-})
+// all_modals = ['addPost-modal', 'randomPost-modal']
+// all_modals.forEach((modal)=>{
+//     const modalSelected = document.querySelector('.'+modal);
+//     modalSelected.classList.remove('fadeIn');
+//     modalSelected.classList.add('fadeOut');
+//     modalSelected.style.display = 'none';
+// })
 
 
-                    // Setup 
-                    const formgif = document.querySelector('#searchbargif');
+//                     // Setup 
+//                     const formgif = document.querySelector('#searchbargif');
 
-                    // Bind Event Listeners
-                    formgif.addEventListener('submit', searchgiphy);
+//                     // Bind Event Listeners
+//                     formgif.addEventListener('submit', searchgiphy);
